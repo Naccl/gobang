468895f621236bf23cd679d3714eaee653d09fc3
Naccl <i@naccl.top>
2021-11-02 18:29:45 +0800

    MQ消息序列化

--- gobang-api/src/main/java/top/naccl/gobang/rabbitmq/MQSender.java
+++ gobang-api/src/main/java/top/naccl/gobang/rabbitmq/MQSender.java
@@ -1,22 +1,18 @@
 package top.naccl.gobang.rabbitmq;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import top.naccl.gobang.config.MQConfig;
 import top.naccl.gobang.model.message.MatchingMQEventMessage;
+import top.naccl.gobang.util.JacksonUtils;
 
 @Service
 public class MQSender {
-	private static final Logger log = LoggerFactory.getLogger(MQSender.class);
-
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
 	public void sent_100(MatchingMQEventMessage matchingMessage) {
-		rabbitTemplate.convertAndSend(MQConfig.TOPIC_EXCHANGE, MQConfig.TOPIC_ROUTINGKEY_100, matchingMessage);
+		rabbitTemplate.convertAndSend(MQConfig.TOPIC_EXCHANGE, MQConfig.TOPIC_ROUTINGKEY_100, JacksonUtils.writeValueAsString(matchingMessage));
 	}
-
 }
